4185a6e058f914b49c60b3cad51d8a30e99f97f6
KattenErMegetSur <111279206+KattenErMegetSur@users.noreply.github.com>
2023-02-28 11:29:34 +0100

    update 1.1v

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import pygame
-import Intro
+img = pygame.image.load("curse.png")
 
-execfile("Intro")
+pygame.init()
+screen_width = 800
+screen_height = 600
+screen = pygame .display.set_mode((screen_width, screen_height))
+screen.blit( img,  (0,0))
+pygame.display.set_caption("Hotel_Dreamer")
+delay = 1
+while True:
+    pygame.display.update()
