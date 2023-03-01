3b3723d839a98fe4d611af854d08b931e93f9236
KattenErMegetSur <111279206+KattenErMegetSur@users.noreply.github.com>
2023-03-01 10:02:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ screen_height = 600
 screen = pygame .display.set_mode((screen_width, screen_height))
 screen.blit( img,  (0,0))
 pygame.display.set_caption("Hotel_Dreamer")
-delay = 1
+
 while True:
     pygame.display.update()
