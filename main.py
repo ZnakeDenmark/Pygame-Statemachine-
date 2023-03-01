fdaf7c1c524c818b1e011ae07975c7c8e07202b8
KattenErMegetSur <111279206+KattenErMegetSur@users.noreply.github.com>
2023-03-01 10:15:13 +0100

    gamming

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
 import pygame
-img = pygame.image.load("curse.png")
 
-pygame.init()
-screen_width = 800
-screen_height = 600
-screen = pygame .display.set_mode((screen_width, screen_height))
-screen.blit( img,  (0,0))
-pygame.display.set_caption("Hotel_Dreamer")
+import sys
+
+import os
+
+from pygame.locals import *
+
+
+pygame.init()  # initialize pygame
+
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((600, 480))
+
+
+# Load the background image here. Make sure the file exists!
+
+bg = pygame.image.load(os.path.join("first floord.png"))
+
+pygame.mouse.set_visible(0)
+
+pygame.display.set_caption('Dream Hotel')
+
+
+# fix indentation
+
 
 while True:
+
+    clock.tick(60)
+
+    screen.blit(bg, (0, 0))
+
+    x, y = pygame.mouse.get_pos()
+
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+
+            sys.exit()
+
+
     pygame.display.update()
