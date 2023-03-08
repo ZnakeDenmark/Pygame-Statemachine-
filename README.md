e6e07b6cfd556da0d81036927545502a04ed6724
ZnakeDenmark <Victor.dyhrman@gmail.com>
2023-03-08 09:59:40 +0100

    edited some files

--- README.md
+++ README.md
@@ -1,10 +1,2 @@
 # Pygame-Statemachine-
-For School project State of Machine
-
-## Current CheckList
-- [ ] Intro
-- [ ] First Floor
-- [ ] Main Menu
-- [ ] Tutorial?
-- [ ] Second floor
-- [ ] Diagram
+For School project StateMachine
--- statetest.py
+++ statetest.py
@@ -11,26 +11,11 @@ class hotelgame(StateMachine):
     start = floor1.to(floor1)
     test = floor1.to(floor2)
 
-    pg.init()
-
-    screen = pg.display.set_mode((1280, 720))
-    clock = pg.time.Clock()
-
 
     @start.on
     def in_floor1(self):
         print("du er i floor1")
-        while True:
-            pressed = pg.key.get_pressed()
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    pg.quit()
-                if event.type == pg.KEYDOWN:
-                    if event.key == pg.K_RETURN:
-                        scene.test()
 
-            pg.display.flip()
-            clock.tick(60)
 
     @test.before
     def before_floor2(self):
