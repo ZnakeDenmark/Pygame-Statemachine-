8372cd175da542dab5e92ef060d43170492c3968
KattenErMegetSur <111279206+KattenErMegetSur@users.noreply.github.com>
2023-03-08 22:43:41 +0100

    gaming

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from sprites import *
 from config import *
 import sys
+from statemachine import statemachine, State
 #import * betyder at det tager hele kode
 class Game:
     def __init__(self):
@@ -9,19 +10,28 @@ class Game:
         self.screen = pygame.display.set_mode((WIN_WIDTH, Win_HEIGHT)) #sætter hvor stort skærm skal være
         self.clock = pygame.time.Clock()
         self.running = True
+        self.font = pygame.font.Font('MOTHEM_Black.ttf', 32)
 
         self.character_spritesheet = Spritesheet('img/character.png')
         self.terrain_spritesheet = Spritesheet('img/terrain.png')
-    
+        self.door_spritesheet = Spritesheet('img/terrain.png')
+        self.enemy_spritesheet = Spritesheet('img/enemy.png')
+        self.attack_spritesheet = Spritesheet('img/attack.png')
+        self.intro_background = pygame.image.load('./img/introbackground.png')
+        self.go_background = pygame.image.load('./img/gameover.png')
+
     def createTilemap(self):
         for i, row in enumerate(tilemap):
             for j, column in enumerate(row):
                 Ground(self, j, i)
+                if column == "D":
+                    Door(self, j, i)
                 if column == "B":
                     Block(self, j, i)
                 if column == "P":
-                    Player(self, j , i)
-
+                    self.player = Player(self, j , i)
+                if column == "E":
+                    Enemy(self, j, i)
 
     def new(self):
         #nyt spil starter
@@ -31,6 +41,7 @@ class Game:
         self.blocks = pygame.sprite.LayeredUpdates()
         self.enemies = pygame.sprite.LayeredUpdates()
         self.attacks = pygame.sprite.LayeredUpdates()
+        self.doors = pygame.sprite.LayeredUpdates()
 
         self.createTilemap()
         
@@ -42,9 +53,21 @@ class Game:
                 self.playing = False
                 self.running = False
 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    if self.player.facing == 'up':
+                        Attack(self, self.player.rect.x, self.player.rect.y - TILESIZE)
+                    if self.player.facing == 'down':
+                        Attack(self, self.player.rect.x, self.player.rect.y + TILESIZE)
+                    if self.player.facing == 'left':
+                        Attack(self, self.player.rect.x - TILESIZE, self.player.rect.y)
+                    if self.player.facing == 'right':
+                        Attack(self, self.player.rect.x + TILESIZE, self.player.rect.y)
+
     def update(self):
         #game loop updates
         self.all_sprites.update()
+        
 
     def draw(self):
         #game loop draw
@@ -59,19 +82,66 @@ class Game:
             self.events()
             self.update()
             self.draw()
-        self.running = False
     
     def game_over(self):
-        pass
+        text = self.font.render('Game Over', True, WHITE)
+        text_rect = text.get_rect(center=(WIN_WIDTH/2, Win_HEIGHT/2))
+
+        restart_button = Button(10, Win_HEIGHT - 60, 120, 50, WHITE, BLACK, 'Restart', 32)
+
+        for sprite in self.all_sprites:
+            sprite.kill()
+
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+            
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_pressed = pygame.mouse.get_pressed()
+
+            if restart_button.is_pressed(mouse_pos, mouse_pressed):
+                self.new()
+                self.main()
+            self.screen.blit(self.go_background, (0,0))
+            self.screen.blit(text, text_rect)
+            self.screen.blit(restart_button.image, restart_button.rect)
+            self.clock.tick(FPS)
+            pygame.display.update()
 
     def intro_screen(self):
-        pass
+        intro = True
+
+        title = self.font.render('Dream Hotel', True, BLACK)
+        title_rect = title.get_rect(x=10, y=10)
+
+        play_button = Button(10, 50, 100, 50, WHITE, BLACK, 'Play', 32)
+
+        while intro:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    intro = False
+                    self.running = False
+
+            mouse_pos = pygame.mouse.get_pos()
+            mouse_pressed = pygame.mouse.get_pressed()
+
+            if play_button.is_pressed(mouse_pos, mouse_pressed):
+                intro = False
+            
+            self.screen.blit(self.intro_background, (0,0))
+            self.screen.blit(title, title_rect)
+            self.screen.blit(play_button.image, play_button.rect)
+            self.clock.tick(FPS)
+            pygame.display.update()
+
 g = Game()
 g.intro_screen()
 g.new()
 while g.running:
     g.main()
     g.game_over()
+
 
 pygame.quit()
 sys.exit()
---
+++ statetest.py
@@ -0,0 +1,25 @@
+from statemachine import StateMachine, State
+
+class level(StateMachine):
+
+
+    "Scene handling"
+    floor1 = State("floor1", initial=True)
+    floor2 = State("floor2")
+
+    start = floor1.to(floor1)
+    next = floor1.to(floor2)
+
+    @start.on
+    def test(self):
+        print("current stage: floor1")
+        scene.next()
+
+    @next.on
+    def test2(self):
+        print("current stage: floor2")
+
+        
+    
+
+scene = level()
