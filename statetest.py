6d15a4800b615ec766cfd9781c26f64fe3744505
KattenErMegetSur <111279206+KattenErMegetSur@users.noreply.github.com>
2023-03-21 10:34:50 +0100

    fixed

--- statetest.py
+++ statetest.py
@@ -1,5 +1,4 @@
 from statemachine import StateMachine, State
-<<<<<<< HEAD
 
 class level(StateMachine):
     "Scene handling"
@@ -31,59 +30,4 @@ class level(StateMachine):
         skift2 = "no more testing"
         level.skift = skift2
         level.lvl = "3"
-
-
-
-        
-    
-
 scene = level()
-=======
-import pygame as pg
-
-class hotelgame(StateMachine):
-    "Scene handling"
-    floor1 = State("floor1", initial=True)
-    floor2 = State("floor2")
-
-
-
-    start = floor1.to(floor1)
-    test = floor1.to(floor2)
-
-    pg.init()
-
-    screen = pg.display.set_mode((1280, 720))
-    clock = pg.time.Clock()
-
-
-    @start.on
-    def in_floor1(self):
-        print("du er i floor1")
-        while True:
-            pressed = pg.key.get_pressed()
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    pg.quit()
-                if event.type == pg.KEYDOWN:
-                    if event.key == pg.K_RETURN:
-                        scene.test()
-
-            pg.display.flip()
-            clock.tick(60)
-
-    @test.before
-    def before_floor2(self):
-        print("Du bevæger dig til floor2")
-
-    @test.on
-    def in_floor2(self):
-        print("Du er i floor2")
-
-scene = hotelgame()
-
-scene.start()
-
-
-
->>>>>>> main
